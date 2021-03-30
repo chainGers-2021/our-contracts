379579e0de75a16891a1669cf8460bd62816bdd9
akcgjc007 <akcgjc007@gmail.com>
2021-03-30 23:49:34 +0530

    refactor+workflow+updates.

--- contracts/Pools/Comptroller.sol
+++ contracts/Pools/Comptroller.sol
@@ -1,39 +1,42 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity >=0.6.0;
 
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import { ILendingPool, ILendingPoolAddressesProvider } from "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
-import '@aave/protocol-v2/contracts/interfaces/IScaledBalanceToken.sol';
-import '../Factory/PrivatePools.sol';
-import { Datatypes } from '../Utils/Datatypes.sol';
+import {
+    ILendingPool,
+    ILendingPoolAddressesProvider
+} from "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
+import "@aave/protocol-v2/contracts/interfaces/IScaledBalanceToken.sol";
+import "../Factory/PrivatePools.sol";
+import {Datatypes} from "../Utils/Datatypes.sol";
 
-contract Comptroller is Ownable
-{
-	using DataTypes for *;
+contract Comptroller is Ownable {
+    using DataTypes for *;
 
-	address public donationPoolContract;
-	address public privatePoolsContract;
-	mapping(string => Datatypes.TokenData) tokenData;
+    address public donationPoolContract;
+    address public privatePoolsContract;
+    mapping(string => Datatypes.TokenData) tokenData;
 
-	event newTokenAdded(string _symbol, address _token, address _aToken);
+    event newTokenAdded(string _symbol, address _token, address _aToken);
 
-	constructor(address _donationPoolContract, address _privatePoolsContract) public
-	{
-		donationPoolContract = _donationPoolContract;
-		privatePoolsContract = _privatePoolsContract;
-	}
+    constructor(address _donationPoolContract, address _privatePoolsContract)
+        public
+    {
+        donationPoolContract = _donationPoolContract;
+        privatePoolsContract = _privatePoolsContract;
+    }
 
-	function addTokenData(
+    function addTokenData(
         string calldata _symbol,
-        address _token, 
-        address _aToken, 
+        address _token,
+        address _aToken,
         address _priceFeed,
         uint8 _decimals
-    ) external onlyOwner
-    {
+    ) external onlyOwner {
         require(
-            keccak256(abi.encode(tokenData[_symbol].symbol)) != keccak256(abi.encode(_symbol)), 
+            keccak256(abi.encode(tokenData[_symbol].symbol)) !=
+                keccak256(abi.encode(_symbol)),
             "Token data already present !"
         );
 
@@ -49,92 +52,106 @@ contract Comptroller is Ownable
         emit newTokenAdded(_symbol, _token, _aToken);
     }
 
-	function depositERC20(
-		string calldata _poolName, 
-		uint256 _amount, 
-		string calldata _tokenSymbol
-	) external 
-	{
-		IERC20 token = IERC20(_token);
-		Datatypes.TokenData storage poolTokenData = tokenData[_tokenSymbol];
-		address lendingPool = ILendingPoolAddressesProvider(lendingPoolAddressProvider).getLendingPool();
+    function depositERC20(
+        string calldata _poolName,
+        uint256 _amount,
+        string calldata _tokenSymbol
+    ) external {
+        IERC20 token = IERC20(_token);
+        Datatypes.TokenData storage poolTokenData = tokenData[_tokenSymbol];
+        address lendingPool =
+            ILendingPoolAddressesProvider(lendingPoolAddressProvider)
+                .getLendingPool();
 
-		// Checking if user has allowed this contract to spend
-		require(
+        // Checking if user has allowed this contract to spend
+        require(
             _amount <= poolTokenData.token.allowance(msg.sender, address(this)),
             "Amount exceeds allowance limit !"
         );
-		// Transfering tokens into this account
-		require(
-            poolTokenData.token.transferFrom(msg.sender, address(this), _amount),
+        // Transfering tokens into this account
+        require(
+            poolTokenData.token.transferFrom(
+                msg.sender,
+                address(this),
+                _amount
+            ),
             "Unable to transfer tokens to comptroller !"
         );
-		// Transfering into Lending Pool
-		require(
-            poolToken.token.approve(lendingPool, _amount), 
+        // Transfering into Lending Pool
+        require(
+            poolToken.token.approve(lendingPool, _amount),
             "Approval failed !"
         );
-		
-		ILendingPool(lendingPool).deposit(
+
+        ILendingPool(lendingPool).deposit(
             poolTokenData.token,
             _amount,
             address(this),
             0
         );
 
-        uint256 reserveNormalizedIncome = ILendingPool(lendingPool).getReserveNormalizedIncome(poolTokenData.token);
-		uint256 newScaledDeposit = (_amount.mul(10**27)).div(reserveNormalizedIncome);
+        uint256 reserveNormalizedIncome =
+            ILendingPool(lendingPool).getReserveNormalizedIncome(
+                poolTokenData.token
+            );
+        uint256 newScaledDeposit =
+            (_amount.mul(10**27)).div(reserveNormalizedIncome);
 
-		uint256 donationAmount = DonationPools(donationPoolContract).donate(
-			newScaledDeposit,
-			_tokenSymbol
-		);
+        uint256 donationAmount =
+            DonationPools(donationPoolContract).donate(
+                newScaledDeposit,
+                _tokenSymbol
+            );
 
-		PrivatePools(privatePoolsContract).deposit(
-			_poolName,
-			newScaledDeposit.sub(donationAmount),
-			_tokenSymbol
-		);
-	}
+        PrivatePools(privatePoolsContract).deposit(
+            _poolName,
+            newScaledDeposit.sub(donationAmount),
+            _tokenSymbol
+        );
+    }
 
-	function withdrawERC20(
-		string calldata _poolName, 
-		uint256 _amount
-	) external
-	{
-		Datatypes.Pool memory tokenSymbol = PrivatePools(privatePoolsContract).poolNames[_poolName].symbol;
+    function withdrawERC20(string calldata _poolName, uint256 _amount)
+        external
+    {
+        Datatypes.Pool memory tokenSymbol =
+            PrivatePools(privatePoolsContract).poolNames[_poolName].symbol;
         Datatypes.TokenData storage poolTokenData = tokenData[tokenSymbol];
-		address lendingPool = ILendingPoolAddressesProvider(lendingPoolAddressProvider).getLendingPool();
-        uint256 reserveNormalizedIncome = ILendingPool(lendingPool).getReserveNormalizedIncome(poolTokenData.token);
-		
-        (uint256 withdrawalAmount, bool penalty) = PrivatePools(privatePoolsContract).withdraw(
-			_poolName, 
-			(_amount.mul(10**27)).div(reserveNormalizedIncome)
-		);
-		
-		// If target price of the pool wasn't achieved, take out the donation amount too.
-		if(penalty)
-		{
-			uint256 donationAmount = DonationPools(donationPoolContract).donate(
-				withdrawalAmount,
-				tokenSymbol
-			);
+        address lendingPool =
+            ILendingPoolAddressesProvider(lendingPoolAddressProvider)
+                .getLendingPool();
+        uint256 reserveNormalizedIncome =
+            ILendingPool(lendingPool).getReserveNormalizedIncome(
+                poolTokenData.token
+            );
 
-			withdrawalAmount = withdrawalAmount.sub(donationAmount);
-		}
-		
-		// Approving aToken pool
+        (uint256 withdrawalAmount, bool penalty) =
+            PrivatePools(privatePoolsContract).withdraw(
+                _poolName,
+                (_amount.mul(10**27)).div(reserveNormalizedIncome)
+            );
+
+        // If target price of the pool wasn't achieved, take out the donation amount too.
+        if (penalty) {
+            uint256 donationAmount =
+                DonationPools(donationPoolContract).donate(
+                    withdrawalAmount,
+                    tokenSymbol
+                );
+
+            withdrawalAmount = withdrawalAmount.sub(donationAmount);
+        }
+
+        // Approving aToken pool
         require(
             IERC20(poolTokenData.aToken).approve(lendingPool, withdrawalAmount),
             "aToken approval failed !"
         );
 
-		// Redeeming the aTokens
+        // Redeeming the aTokens
         ILendingPool(lendingPool).withdraw(
-            poolTokenData.token, 
-            withdrawalAmount, 
+            poolTokenData.token,
+            withdrawalAmount,
             msg.sender
         );
-	}
-	
+    }
 }
--- test/2_test.js
+++ test/2_test.js
@@ -4,7 +4,7 @@ const Web3 = require("web3");
 const hre = require("hardhat");
 const truffleAssert = require("truffle-assertions");
 const web3 = new Web3(hre.network.provider);
-const kovan = require("./../info/kovan.json");
+const kovan = require("../info/kovan.json");
 const linkTokenAddress = kovan[6].address;
 const tokenAddr = kovan[6].address;
 const aLinkTokenAddress = kovan[6].aTokenAddress;
@@ -17,21 +17,24 @@ const compiledILendingPool = require("@aave/protocol-v2/artifacts/contracts/inte
 const compiledIScaledBalanceToken = require("@aave/protocol-v2/artifacts/contracts/interfaces/IScaledBalanceToken.sol/IScaledBalanceToken.json");
 
 beforeEach(async () => {
-    accounts = await web3.eth.getAccounts();
-    admin = accounts[0];
-    user = accounts[1];
-    console.log("Ether balance of admin: ", await web3.eth.getBalance(admin));
-    console.log("Ether balance of user: ", await web3.eth.getBalance(user));
+  accounts = await web3.eth.getAccounts();
+  admin = accounts[0];
+  user = accounts[1];
+  console.log("Ether balance of admin: ", await web3.eth.getBalance(admin));
+  console.log("Ether balance of user: ", await web3.eth.getBalance(user));
 
-    tokenAllowLimit = BigInt(100 * inWei);
-    sendAmount1 = BigInt(1 * inWei);
-    sendAmount2 = BigInt(0.001 * inWei);
-    aTokenAllowLimit = BigInt(1 * inWei);
-    uintMax = BigInt(2 ** 256 - 1);
+  tokenAllowLimit = BigInt(100 * inWei);
+  sendAmount1 = BigInt(1 * inWei);
+  sendAmount2 = BigInt(0.001 * inWei);
+  aTokenAllowLimit = BigInt(1 * inWei);
+  uintMax = BigInt(2 ** 256 - 1);
 
-    aLinkTokenAddressContract = new web3.eth.Contract(tokenABI, aLinkTokenAddress);
+  aLinkTokenAddressContract = new web3.eth.Contract(
+    tokenABI,
+    aLinkTokenAddress
+  );
 
-    await aLinkTokenAddressContract.methods
+  await aLinkTokenAddressContract.methods
     .balanceOf(admin)
     .call()
     .then((res) => {
@@ -39,169 +42,216 @@ beforeEach(async () => {
       currentBalance = res;
     });
 
-    // process.exit(0);
+  // process.exit(0);
 
-    // Deploying Factory
-    compiledFactoryV2 = await hre.artifacts.readArtifact("PrivatePools");
-    factoryV2 = await new web3.eth.Contract((await compiledFactoryV2).abi)
-        .deploy({
-            data: (await compiledFactoryV2).bytecode,
-        })
-        .send({
-            from: admin,
-        })
-    
-    console.log("Deployed at:", factoryV2.options.address);
-    // console.log("Admin address is owner address: ", admin === await factoryV2.methods.checkOwner().call());
-    // console.log("User address is owner address: ", user === await factoryV2.methods.checkOwner().call());
+  // Deploying Factory
+  compiledFactoryV2 = await hre.artifacts.readArtifact("PrivatePools");
+  factoryV2 = await new web3.eth.Contract((await compiledFactoryV2).abi)
+    .deploy({
+      data: (await compiledFactoryV2).bytecode,
+    })
+    .send({
+      from: admin,
+    });
 
-    await factoryV2.methods.addTokenData("LINK", linkTokenAddress, aLinkTokenAddress, "0x396c5E36DD0a0F5a5D33dae44368D4193f69a1F0", 8)
-        .send({ from: admin })
-        .then(tx=>console.log(tx));
-    // Creating a account address and corresponding private key for a new pool
-    newAccountData = await web3.eth.accounts.create();
-    // console.log("Pool exists: ", await factoryV2.methods.verifyPool("TEST").call());
-    // await factoryV2.methods.isPoolEmpty("TEST").call()
-    // .then(tx=>console.log("Pool name: ", tx));
-    // Create a new pool.
-    await factoryV2.methods.createPool("LINK", "TEST", 1000, newAccountData.address)
-        .send({ from: admin, gas: 10**7 })
+  console.log("Deployed at:", factoryV2.options.address);
+  // console.log("Admin address is owner address: ", admin === await factoryV2.methods.checkOwner().call());
+  // console.log("User address is owner address: ", user === await factoryV2.methods.checkOwner().call());
 
-    console.log("Pool exists: ", await factoryV2.methods.verifyPool("TEST").call());
-    
-    // await factoryV2.methods.isPoolEmpty("TEST").call()
-    //     .then(tx=>console.log("Pool name: ", tx));
+  await factoryV2.methods
+    .addTokenData(
+      "LINK",
+      linkTokenAddress,
+      aLinkTokenAddress,
+      "0x396c5E36DD0a0F5a5D33dae44368D4193f69a1F0",
+      8
+    )
+    .send({ from: admin })
+    .then((tx) => console.log(tx));
+  // Creating a account address and corresponding private key for a new pool
+  newAccountData = await web3.eth.accounts.create();
+  // console.log("Pool exists: ", await factoryV2.methods.verifyPool("TEST").call());
+  // await factoryV2.methods.isPoolEmpty("TEST").call()
+  // .then(tx=>console.log("Pool name: ", tx));
+  // Create a new pool.
+  await factoryV2.methods
+    .createPool("LINK", "TEST", 1000, newAccountData.address)
+    .send({ from: admin, gas: 10 ** 7 });
 
-    // process.exit(0);
-    // Generate a random string. This string is signed using the privateKey.
-    randomHexString = await web3.utils.randomHex(32);
-    signObject = await web3.eth.accounts.sign(randomHexString, newAccountData.privateKey);
+  console.log(
+    "Pool exists: ",
+    await factoryV2.methods.verifyPool("TEST").call()
+  );
 
-    // Send the hashMessage of the signObject along with the r, s and v values of the signObject to the private pool via factory contract.
-    await factoryV2.methods.verifyPoolAccess("TEST", signObject.messageHash, signObject.signature)
-        .send({ from: user });
+  // await factoryV2.methods.isPoolEmpty("TEST").call()
+  //     .then(tx=>console.log("Pool name: ", tx));
 
-    await factoryV2.methods.getVerifiedStatus("TEST")
-        .call({ from: user })
-        .then((result) => {
-            console.log("User is verified: ", result);
-        });
+  // process.exit(0);
+  // Generate a random string. This string is signed using the privateKey.
+  randomHexString = await web3.utils.randomHex(32);
+  signObject = await web3.eth.accounts.sign(
+    randomHexString,
+    newAccountData.privateKey
+  );
 
+  // Send the hashMessage of the signObject along with the r, s and v values of the signObject to the private pool via factory contract.
+  await factoryV2.methods
+    .verifyPoolAccess("TEST", signObject.messageHash, signObject.signature)
+    .send({ from: user });
+
+  await factoryV2.methods
+    .getVerifiedStatus("TEST")
+    .call({ from: user })
+    .then((result) => {
+      console.log("User is verified: ", result);
+    });
 });
 
 describe("Deposit process", async function () {
-    it("Should be able to create pools", async () => {
-        // Create a new account. It returns an account address and its corresponding privateKey.
-        newAccountData = await web3.eth.accounts.create();
+  it("Should be able to create pools", async () => {
+    // Create a new account. It returns an account address and its corresponding privateKey.
+    newAccountData = await web3.eth.accounts.create();
 
-        // Create a new pool.
-        await factoryV2.methods.createPool(linkTokenAddress, "TEST", 50, newAccountData.address)
-            .send({ from: admin });
-        console.log("Pool exists: ", await factoryV2.methods.verifyPool("TEST").call());
+    // Create a new pool.
+    await factoryV2.methods
+      .createPool(linkTokenAddress, "TEST", 50, newAccountData.address)
+      .send({ from: admin });
+    console.log(
+      "Pool exists: ",
+      await factoryV2.methods.verifyPool("TEST").call()
+    );
 
-        // Generate a random string. This string is signed using the privateKey.
-        randomHexString = await web3.utils.randomHex(32);
-        signObject = await web3.eth.accounts.sign(randomHexString, newAccountData.privateKey);
+    // Generate a random string. This string is signed using the privateKey.
+    randomHexString = await web3.utils.randomHex(32);
+    signObject = await web3.eth.accounts.sign(
+      randomHexString,
+      newAccountData.privateKey
+    );
 
-        // Send the hashMessage of the signObject along with the r, s and v values of the signObject to the private pool via factory contract.
-        await factoryV2.methods.verifyPoolAccess("TEST", signObject.messageHash, signObject.signature)
-            .send({ from: user });
+    // Send the hashMessage of the signObject along with the r, s and v values of the signObject to the private pool via factory contract.
+    await factoryV2.methods
+      .verifyPoolAccess("TEST", signObject.messageHash, signObject.signature)
+      .send({ from: user });
 
-        await factoryV2.methods.getVerifiedStatus("TEST")
-            .call({ from: user })
-            .then((result) => {
-                console.log("User is verified: ", result);
-            });
-    });
+    await factoryV2.methods
+      .getVerifiedStatus("TEST")
+      .call({ from: user })
+      .then((result) => {
+        console.log("User is verified: ", result);
+      });
+  });
 
-    it.only("Should allow users to deposit", async () => {
-        token = new web3.eth.Contract(tokenABI, tokenAddr);
-        
-        // Approving pool to spend ERC20
-        await token.methods
-        .approve(factoryV2.options.address, sendAmount1)
-        .send({ from: user })
-        .then((tx) => console.log("Approve1: ", tx.gasUsed));
-        
-        await aLinkTokenAddressContract.methods
-        .balanceOf(user)
-        .call()
-        .then((res) => {
-            console.log("User ERC20 balance before deposit: ", res);
-        });
+  it.only("Should allow users to deposit", async () => {
+    token = new web3.eth.Contract(tokenABI, tokenAddr);
 
-        await factoryV2.methods.depositERC20("TEST", sendAmount1)
-        .send({ from: user, gas: 10**7 })
-        .then((tx) => console.log("depositERC20(): ", tx.gasUsed));
-        
-        await aLinkTokenAddressContract.methods
-        .balanceOf(user)
-        .call()
-        .then((res) => {
-            console.log("User ERC20 balance after deposit: ", res);
+    // Approving pool to spend ERC20
+    await token.methods
+      .approve(factoryV2.options.address, sendAmount1)
+      .send({ from: user })
+      .then((tx) => console.log("Approve1: ", tx.gasUsed));
 
-        });
+    await aLinkTokenAddressContract.methods
+      .balanceOf(user)
+      .call()
+      .then((res) => {
+        console.log("User ERC20 balance before deposit: ", res);
+      });
 
-        _aToken = new web3.eth.Contract(compiledIScaledBalanceToken.abi, aLinkTokenAddress);
+    await factoryV2.methods
+      .depositERC20("TEST", sendAmount1)
+      .send({ from: user, gas: 10 ** 7 })
+      .then((tx) => console.log("depositERC20(): ", tx.gasUsed));
 
-        // Scaled balance
-        await _aToken.methods.scaledBalanceOf(factoryV2.options.address)
-        .call()
-        .then(a => { console.log("Scaled balance: ", a);});
+    await aLinkTokenAddressContract.methods
+      .balanceOf(user)
+      .call()
+      .then((res) => {
+        console.log("User ERC20 balance after deposit: ", res);
+      });
 
-        let userScaled;
-        await factoryV2.methods.getUserScaledDeposit("TEST").call({ from: user })
-        .then((tx)=>{
-            console.log("Scaled Balance from contract: ", tx);
-            userScaled = tx;
-        }); 
-        
-        await factoryV2.methods.getUserScaledBalance("TEST").call({ from: user })
-        .then((tx)=>{ 
-            console.log("aToken amount from contract: ", tx);
-        });
-        
-        await factoryV2.methods.getReserveData("TEST").call()
-        .then((tx)=>{
-            console.log("Liquidity index before withdrawal: ", tx[0]);
-            console.log("Reserve normalized income: ", tx[1]);
-            console.log("aToken amount to be withdrawn (LI): ", (tx[0]*userScaled)/10**27);
-            console.log("aToken amount to be withdrawn (RNI): ", (tx[1]*userScaled)/10**27);
-        });
+    _aToken = new web3.eth.Contract(
+      compiledIScaledBalanceToken.abi,
+      aLinkTokenAddress
+    );
 
-        
-        // await factoryV2.methods.getReserveNormalizedIncome("TEST").call()
-        // .then(tx=> console.log("Reserve normalized income: ", tx));
+    // Scaled balance
+    await _aToken.methods
+      .scaledBalanceOf(factoryV2.options.address)
+      .call()
+      .then((a) => {
+        console.log("Scaled balance: ", a);
+      });
 
-        await factoryV2.methods.withdrawERC20("TEST", 0)
-        .send({ from: user, gas: 10**7 });
+    let userScaled;
+    await factoryV2.methods
+      .getUserScaledDeposit("TEST")
+      .call({ from: user })
+      .then((tx) => {
+        console.log("Scaled Balance from contract: ", tx);
+        userScaled = tx;
+      });
 
-        await _aToken.methods.scaledBalanceOf(factoryV2.options.address)
-        .call()
-        .then(a => { console.log("Scaled balance after withdrawal: ", a);});
+    await factoryV2.methods
+      .getUserScaledBalance("TEST")
+      .call({ from: user })
+      .then((tx) => {
+        console.log("aToken amount from contract: ", tx);
+      });
 
-        await factoryV2.methods.getUserScaledDeposit("TEST").call({ from: user })
-        .then(tx=>console.log("User scaled balance after withdrawal: ", tx));
+    await factoryV2.methods
+      .getReserveData("TEST")
+      .call()
+      .then((tx) => {
+        console.log("Liquidity index before withdrawal: ", tx[0]);
+        console.log("Reserve normalized income: ", tx[1]);
+        console.log(
+          "aToken amount to be withdrawn (LI): ",
+          (tx[0] * userScaled) / 10 ** 27
+        );
+        console.log(
+          "aToken amount to be withdrawn (RNI): ",
+          (tx[1] * userScaled) / 10 ** 27
+        );
+      });
 
-        await factoryV2.methods.getPoolScaledAmount("TEST").call()
-        .then(tx=>console.log("Pool scaled amount: ", tx));
+    // await factoryV2.methods.getReserveNormalizedIncome("TEST").call()
+    // .then(tx=> console.log("Reserve normalized income: ", tx));
 
-        await aLinkTokenAddressContract.methods
-        .balanceOf(user)
-        .call()
-        .then((res) => {
-            console.log("User ERC20 balance after withdraw: ", res);
-            currentBalance = res;
-        });
+    await factoryV2.methods
+      .withdrawERC20("TEST", 0)
+      .send({ from: user, gas: 10 ** 7 });
 
-        await aLinkTokenAddressContract.methods
-        .balanceOf(factoryV2.options.address)
-        .call()
-        .then((res) => {
-            console.log("Contract ERC20 balance after withdraw: ", res);
-            currentBalance = res;
-        });
+    await _aToken.methods
+      .scaledBalanceOf(factoryV2.options.address)
+      .call()
+      .then((a) => {
+        console.log("Scaled balance after withdrawal: ", a);
+      });
 
-    });
+    await factoryV2.methods
+      .getUserScaledDeposit("TEST")
+      .call({ from: user })
+      .then((tx) => console.log("User scaled balance after withdrawal: ", tx));
 
+    await factoryV2.methods
+      .getPoolScaledAmount("TEST")
+      .call()
+      .then((tx) => console.log("Pool scaled amount: ", tx));
+
+    await aLinkTokenAddressContract.methods
+      .balanceOf(user)
+      .call()
+      .then((res) => {
+        console.log("User ERC20 balance after withdraw: ", res);
+        currentBalance = res;
+      });
+
+    await aLinkTokenAddressContract.methods
+      .balanceOf(factoryV2.options.address)
+      .call()
+      .then((res) => {
+        console.log("Contract ERC20 balance after withdraw: ", res);
+        currentBalance = res;
+      });
+  });
 });
