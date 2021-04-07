75161c01a561ed314527286af6ec5e0220e7f915
shantanhunt <shantanusontakke015@gmail.com>
2021-04-07 06:02:45 -0700

    Require testing public pools

--- test/1_test_basic.js
+++ test/1_test_basic.js
@@ -28,74 +28,135 @@ contract("--PublicPools testing--", async (accounts) => {
     pvt = await PrivatePools.new(comp.address);
     pub = await PublicPools.new(comp.address);
     don = await DonationPools.new(comp.address);
-    // comp = await Comptroller.deployed();
-    // pvt = await PrivatePools.deployed();
-    // pub = await PublicPools.deployed();
-    // don = await DonationPools.deployed();
 
-    console.log(await comp.setPoolAddresses(
+    const setPoolAddresses = await comp.setPoolAddresses(
       pvt.address,
       pub.address,
       don.address
-    ));
+    );
 
-    console.log(await comp.addTokenData(
+    const addTokenData = await comp.addTokenData(
       "LINK",
       "0xad5ce863ae3e4e9394ab43d4ba0d80f419f61789",
       "0xeD9044cA8F7caCe8eACcD40367cF2bee39eD1b04",
       "0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c",
       8
-    ));
+    );
 
     // Adding a recipient of the donation amounts
-    await don.addRecipient(
+    const addRecipient = await don.addRecipient(
       admin,
       "DEV"
     );
 
     // Creating public pool
-    console.log(await pub.createPool("LINK", "Test1", 45, {
+    const createPool = await pub.createPool("LINK", "Test1", 45, {
       from: admin,
-    }));
+    });
+    console.log(createPool.logs);
 
     for (i = 1; i < 5; i++) {
-      console.log(await link.transfer(accounts[i], toWei(10)));
-      console.log(await web3.eth.sendTransaction({ to: accounts[i], from: admin, value: toWei("0.5") }))
+      const transferLink = await link.transfer(accounts[i], toWei(10));
+      const sendtx = await web3.eth.sendTransaction({ to: accounts[i], from: admin, value: toWei("0.5") });
+    }
+
+    //Initial balances
+    console.log("Initial balances: ");
+    for (i = 1; i < 5; i++) {
+      console.log("LINK balance of ", i, " : ", parseInt(await link.balanceOf(accounts[i])));
     }
   });
 
-  it("allows users to deposit LINK", async () => {
+  it("Cannot create Pool with no name", async () => {
+    const createNamelessPool = await pub.createPool("LINK", "", 45, {
+      from: admin,
+    });
+    //console.log(createNamelessPool.logs);
+    assert.isUndefined(pub.poolNames[""],"No pool with blank name exists");
+  });
+
+  // it("Only owner can create the Pool", async () => {
+  //     try {
+  //       await pub.createPool("LINK", "Test2", 45, {
+  //         from: user1,
+  //         });
+  //     } 
+  //     catch (err) {
+  //       assert.exists(err.message);
+  //     }
+  // });
+
+  it("Cannot create Pool with no token symbol", async () => {
+    await pub.createPool("", "Test3", 45, {
+      from: admin,
+    });
+    assert.isUndefined(pub.poolNames["Test3"],"Pool with blank symbol not exists");
+  });
+
+  it("Cannot create Pool with same name", async () => {
+    try {
+      console.log("Creating Duplicate Pool...");
+      await pub.createPool("LINK", "Test1", 45, {
+      from: admin,
+      });
+    } 
+    catch (err) {
+      assert.exists(err);
+      console.log(err+ "As duplicate pool cannot be created");
+      }
+  });
+
+  //-------------------TO DO------------------
+  //Error is not logged after creating a pool as such below
+  //Need to write code to do that
+  it("Cannot create Pool with same name but different token symbol", async () => {
+    try {
+      console.log("Creating Duplicate Pool with ETH token...");
+      createDuplicatePool = await pub.createPool("ETH", "Test1", 45, {
+      from: admin,
+      });
+      console.log(createDuplicatePool);
+    } 
+    catch (err) {
+      assert.exists(err);
+      console.log(err+ "As duplicate pool with different token cannot be created");
+      }
+  });
+
+  it("Allows users to deposit LINK", async () => {
     // depositing into a pool
+    console.log("Balances after Deposit: ")
     for (i = 1; i < 5; i++) {
-      console.log(await link.approve(comp.address, toWei(1), { from: accounts[i] }));
-      console.log(await comp.depositERC20("Test1", toWei(1), "LINK", false, { from: accounts[i] }));
-      console.log("User scaled amount: ", (await pub.getUserScaledDeposit("Test1", { from: accounts[i] })).toString());
+      const linkApprove = await link.approve(comp.address, toWei(1), { from: accounts[i] });
+      const depositERC20 = await comp.depositERC20("Test1", toWei(1), "LINK", false, { from: accounts[i] });
+      const userScaledAmount =  (await pub.getUserScaledDeposit("Test1", { from: accounts[i] })).toString();
+      console.log("User scaled Amount: ", userScaledAmount);
       console.log("LINK balance of ", i, " : ", parseInt(await link.balanceOf(accounts[i])));
     }
-
   });
 
   it("allows users to withdraw their stake(LINK)", async () => {
     // state change
     // console.log(await pub.breakPool("Test1", {from: admin}));
-
+    console.log("Balances after withdrawal: ");
     for (i = 1; i < 5; ++i) {
       await comp.withdrawERC20("Test1", toWei(0.5), false, { from: accounts[i] });
-      console.log("User scaled amount: ", (await pub.getUserScaledDeposit("Test1", { from: accounts[i] })).toString());
+      const userScaledAmount = (await pub.getUserScaledDeposit("Test1", { from: accounts[i] })).toString();
+      console.log("User Scaled Amount: ", userScaledAmount);
       console.log("LINK balance of ", i, " : ", parseInt(await link.balanceOf(accounts[i])));
     }
     console.log("LINK balance of admin: ", parseInt(await link.balanceOf(admin)));
 
-    iscal = await IScaledBalanceToken.at(aLinkAddress);
+    const iscal = await IScaledBalanceToken.at(aLinkAddress);
     console.log("Pool scaled balance: ", parseInt(await iscal.scaledBalanceOf(comp.address)));
   });
 
-  it("Allows recipients of donation pools to withdraw their stake", async () => {
-    await comp.withdrawDonation("LINK");
-    console.log("Donation withdrawal successfull");
-    console.log("LINK balance of admin: ", parseInt(await link.balanceOf(admin)));
-    console.log("Pool scaled balance: ", parseInt(await iscal.scaledBalanceOf(comp.address)));
-  });
+  // it("Allows recipients of donation pools to withdraw their stake", async () => {
+  //   await comp.withdrawDonation("LINK");
+  //   console.log("Donation withdrawal successfull");
+  //   console.log("LINK balance of admin: ", parseInt(await link.balanceOf(admin)));
+  //   console.log("Pool scaled balance: ", parseInt(await iscal.scaledBalanceOf(comp.address)));
+  // });
 
   console.log("--End of PublicPools Testing");
 });
